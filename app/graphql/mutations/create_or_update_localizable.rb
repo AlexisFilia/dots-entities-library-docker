1fce5fd0e9b88500efa53c76b613e41f819a3c5b
Alexis Filia <58165550+AlexisFilia@users.noreply.github.com>
2022-01-10 15:54:24 +0100

    Problème avec localized

--- app/graphql/mutations/create_or_update_localizable.rb
+++ app/graphql/mutations/create_or_update_localizable.rb
@@ -7,9 +7,23 @@ module Mutations
 
     def resolve(attributes:, id: nil)
       model = find_or_build_model(id)
-      localizable_type = attributes[:localizable_type].upcase_first
-      model.attributes = attributes.to_h.except(:localizable_type)
-      model.localizable_type = localizable_type
+      if id
+        unless attributes[:localizable_type].blank? && attributes[:localizable_id].blank? && attributes[:language].blank? && attributes[:type_of].blank?
+          raise GraphQL::ExecutionError,
+                "typeOf, language, localizableId and localizableType can't be updated, they need to be blank"
+        end
+        model.value = attributes[:value]
+
+      else
+        if attributes[:localizable_type].blank? || attributes[:localizable_id].blank? || attributes[:value].blank? || attributes[:language].blank? || attributes[:type_of].blank?
+          raise GraphQL::ExecutionError,
+                "typeOf, language, value, localizableId and localizableType can't be blank for a creation"
+        end
+
+        localizable_type = attributes[:localizable_type].upcase_first
+        model.attributes = attributes.to_h.except(:localizable_type)
+        model.localizable_type = localizable_type
+      end
 
       model if model.save!
     end
