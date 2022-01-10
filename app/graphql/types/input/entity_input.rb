80a8d67046854d2f7f8d0ff91bc3801b5bdffafe
Alexis Filia <58165550+AlexisFilia@users.noreply.github.com>
2022-01-10 17:20:17 +0100

    Ajout de child_order dans les schema GraphQL

--- app/graphql/types/input/entity_input.rb
+++ app/graphql/types/input/entity_input.rb
@@ -2,6 +2,7 @@ module Types
   module Input
     class EntityInput < Types::BaseInputObject
       argument :name, String, required: false
+      argument :child_order, [ID], required: false
       argument :default_fields, [ID], required: false
       argument :default_actions, [ID], required: false
       argument :parent_id, ID, required: false
--- app/graphql/types/input/section_input.rb
+++ app/graphql/types/input/section_input.rb
@@ -2,6 +2,7 @@ module Types
   module Input
     class SectionInput < Types::BaseInputObject
       argument :name, String, required: false
+      argument :child_order, [ID], required: false
       argument :entity_id, ID, required: false
       argument :element_type, Types::Enum::SectionTypeEnumType, required: false
     end
