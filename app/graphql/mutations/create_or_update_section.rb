e863518623427666ddceb177f0ed13584fb3f566
Alexis Filia <58165550+AlexisFilia@users.noreply.github.com>
2022-01-11 16:55:38 +0100

    Correction de CreateOrUpdateSection

--- app/graphql/mutations/create_or_update_section.rb
+++ app/graphql/mutations/create_or_update_section.rb
@@ -6,22 +6,21 @@ module Mutations
     type Types::SectionType
 
     def resolve(attributes:, id: nil)
-      model = find_or_build_model(id)
-      unless model.id.blank? || model.entity_id == attributes[:entity_id].to_i || attributes[:entity_id].blank?
-        raise GraphQL::ExecutionError,
-              "You can't link this section to an other entity"
-      end
-      unless model.element_type.blank? || model.element_type == attributes[:element_type] || attributes[:element_type].blank?
-        raise GraphQL::ExecutionError,
-              "You can't change the element_type once it's defined"
-      end
-      if model.name == 'fieldsRootSection' || model.name == 'actionsRootSection'
-        raise GraphQL::ExecutionError,
-              "You can't changeb 'fieldsRootSection' or 'actionsRootSection' name"
-      end
-      if attributes[:name] == 'fieldsRootSection' || attributes[:name] == 'actionsRootSection'
+      if id
+        unless attributes[:entity_id].blank? || attributes[:element_type].blank?
+          raise GraphQL::ExecutionError,
+                "You can't update entity_id and element_type"
+        end
+      elsif attributes[:name] == 'fieldsRootSection' || attributes[:name] == 'actionsRootSection'
         raise GraphQL::ExecutionError,
               "You can't use 'fieldsRootSection' or 'actionsRootSection' as a name for your sections"
+      end
+
+      model = find_or_build_model(id)
+
+      if (model.name == 'fieldsRootSection' || model.name == 'actionsRootSection') && !attributes[:name].blank?
+        raise GraphQL::ExecutionError,
+              "You can't change 'fieldsRootSection' or 'actionsRootSection' name"
       end
 
       model.attributes = attributes.to_h
